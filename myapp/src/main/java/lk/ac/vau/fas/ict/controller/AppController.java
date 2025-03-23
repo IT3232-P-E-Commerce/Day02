20eb024d9721ba1471803255e7fd49a63f1d55e1
kaveeshabasnayaka <kaveeshabasnayaka9@gmail.com>
2025-03-23 22:31:50 +0530

    create first msg

---
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -0,0 +1,14 @@
+package lk.ac.vau.fas.ict.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+	@GetMapping("/msg")
+	public String myMsg() {
+		return "Hello springboot";
+	}
+}
